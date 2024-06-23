8b115b6b7888b8a191d83d45245b4445f1d5e1b7
dariolggomez <dariogomez1011@gmail.com>
2024-06-22 23:19:28 -0400

    create post component

---
+++ src/components/Post.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Post() {
+    return (
+        <div>
+            <p>Dario</p>
+            <p>First post!</p>
+        </div>
+    );
+}
+
+export default Post;
