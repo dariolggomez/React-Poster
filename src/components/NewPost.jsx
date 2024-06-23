c41369ef96cc8c5000b4545247a3144cca501780
dariolggomez <dariogomez1011@gmail.com>
2024-06-23 18:24:27 -0400

    handle form data on new post

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,15 +1,37 @@
 import classes from './NewPost.module.css';
+import { useState } from 'react';
 
-function NewPost({onBodyChange, onAuthorChange, onHideModal}) {
+function NewPost({onHideModal}) {
+  const [enteredBody, setEnteredBody] = useState('');
+  const [enteredAuthor, setEnteredAuthor] = useState('');
+
+  function changeBodyHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function changeAuthorHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
+  function submitHandler(event) {
+    event.preventDefault();
+    const formData = {
+      body: event.target.body.value,
+      name: event.target.name.value,
+    }
+    console.log(formData);
+    onHideModal();
+  }
+
   return (
-    <form className={classes.form}>
+    <form onSubmit={submitHandler} className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={onBodyChange} />
+        <textarea id="body" required rows={3} onChange={changeBodyHandler} />
       </p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required onChange={onAuthorChange} autoComplete='off' />
+        <input type="text" id="name" required onChange={changeAuthorHandler} autoComplete='off' />
       </p>
       <p className={classes.actions}>
         <button onClick={onHideModal} type='button'>Cancel</button>
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -5,27 +5,17 @@ import Modal from "./Modal";
 import { useState } from "react";
 
 function PostsList({hideModalHandler, renderModal}) {
-  const [enteredBody, setEnteredBody] = useState('');
-  const [enteredAuthor, setEnteredAuthor] = useState('');
-
-  function changeBodyHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-
-  function changeAuthorHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
 
   return (
     <>
       {
         renderModal && 
         <Modal onHideModal={hideModalHandler} >
-          <NewPost onHideModal={hideModalHandler} onBodyChange={changeBodyHandler} onAuthorChange={changeAuthorHandler} />
+          <NewPost onHideModal={hideModalHandler} />
         </Modal>
       }
       <ul className={classes.posts}>
-        <Post author={enteredAuthor} body={enteredBody} />
+        
       </ul>
     </>
   );
