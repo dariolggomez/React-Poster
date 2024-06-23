5e3c53658399f7166689c0085dc6f750393f9cc8
dariolggomez <dariogomez1011@gmail.com>
2024-06-23 00:15:04 -0400

    create initial css styles for posts list component

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,8 +1,9 @@
 import Post from "./Post";
+import classes from "./PostsList.module.css";
 
 function PostsList() {
     return (
-        <ul>
+        <ul className={classes.posts}>
             <Post author="Dario" body="First Post!" />
         </ul>
     );
