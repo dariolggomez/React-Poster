8c1ede2006ecc9c019cd3ddd3aa19d14ecb93727
dariolggomez <dariogomez1011@gmail.com>
2024-06-23 22:04:53 -0400

    show loading while fetching posts

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -12,17 +12,20 @@ function PostsList({ hideModalHandler, renderModal }) {
     renderModal: PropTypes.bool.isRequired,
   };
   const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
 
   useEffect(() => {
     async function fetchPosts() {
+      setIsFetching(true);
       const response = await fetch("http://localhost:8080/posts");
       const data = await response.json();
       setPosts(data.posts);
+      setIsFetching(false);
     }
 
     fetchPosts();
   }, []);
-  
+
   return (
     <>
       {
@@ -31,16 +34,19 @@ function PostsList({ hideModalHandler, renderModal }) {
           <NewPost onHideModal={hideModalHandler} />
         </Modal>
       }
-      {posts.length > 0 && <ul className={classes.posts}>
+      {!isFetching && posts.length > 0 && <ul className={classes.posts}>
         {posts.map((post) => (
           <Post key={post.body} author={post.author} body={post.body} />
         ))}
       </ul>}
-      {posts.length === 0 &&
+      {!isFetching && posts.length === 0 &&
         <div style={{ textAlign: 'center', color: 'white' }}>
           <h2>There are no posts yet.</h2>
           <p>Start adding some!</p>
         </div>}
+      {isFetching && <div style={{ textAlign: 'center', color: 'white' }}>
+        <p>Loading posts...</p>
+      </div>}
     </>
   );
 }
